91e7dea3325e7eb8e0eceaf92ce9a100cf280fb8
Dan137s <71358040+Dan137s@users.noreply.github.com>
2021-04-25 13:37:38 -0400

    v.09 Menú con botones enlaces + validaciones

--- app/src/main/java/com/claseipst51android2021cl/grupo4/MainActivitySesion.java
+++ app/src/main/java/com/claseipst51android2021cl/grupo4/MainActivitySesion.java
@@ -2,13 +2,17 @@ package com.claseipst51android2021cl.grupo4;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivitySesion extends AppCompatActivity {
+    //Son los campos de correo y contraseña para post validar
+    EditText CampoCorreo, CampoContraseña;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,23 +22,52 @@ public class MainActivitySesion extends AppCompatActivity {
         //Titulo de la aplicacion o action bar que lo centre
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.txt_titulo);
-    }
-    public void ingresar(View view) {
-        Intent i1 = new Intent(this, MenuActivity.class);
-        Toast.makeText(getApplicationContext(), "Bienvenidos a ecos",
-                Toast.LENGTH_SHORT).show();
-        startActivity(i1);
 
+        //Parsear los campos con el id correspondiente
+        CampoCorreo = (EditText) findViewById(R.id.txtCorreoSesion);
+        CampoContraseña = (EditText) findViewById(R.id.txtContraseñaSesion);
     }
+
+
+
+    //Cree 2 variables para devolver un verdadero o falso con la cadena de texto dentro de un if
+    public boolean validar() {
+        boolean retorno = true;
+        String c1 = CampoCorreo.getText().toString();
+        String c2 = CampoContraseña.getText().toString();
+        if(c1.isEmpty())
+        {
+            CampoCorreo.setError("El campo correo no debe estar vacio");
+            retorno=false;
+        }
+        if(c2.isEmpty())
+        {
+            CampoContraseña.setError("El campo contraseña no debe estar vacio");
+            retorno=false;
+        }
+        return retorno;
+    }
+
+
+    //Btn ingresar y volver el btn ingresar valida y ejecuta mediante el boton el metodo validar
+    public void ingresar(View v)
+    {
+        if(validar())
+        {
+            Intent is1 = new Intent(this, MenuActivity.class);
+            Toast.makeText(getApplicationContext(), "Bienvenidos a ecos",
+                    Toast.LENGTH_SHORT).show();
+            startActivity(is1);
+        }
+    }
+    //Este me devuelve a la pestaña anterior osea al inicio
     public void regresar(View view){
-        Intent i2 = new Intent(this, MainActivityLogin.class);
+        Intent is2 = new Intent(this, MainActivityLogin.class);
         Toast.makeText(getApplicationContext(), "Back inicio",
                 Toast.LENGTH_SHORT).show();
-        startActivity(i2);
-        //
+        startActivity(is2);
     }
 }
-
 
 
 
