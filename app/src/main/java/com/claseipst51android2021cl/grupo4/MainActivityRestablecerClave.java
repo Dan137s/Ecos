b174f7cb7a897443f78be560e932b9c07895cd0c
Dan137s <71358040+Dan137s@users.noreply.github.com>
2021-04-30 02:19:43 -0400

    V.1.15 Login + Restablecer contraseña + firebase

--- app/src/main/java/com/claseipst51android2021cl/grupo4/MainActivityRestablecerClave.java
+++ app/src/main/java/com/claseipst51android2021cl/grupo4/MainActivityRestablecerClave.java
@@ -4,44 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-import com.skydoves.elasticviews.ElasticCheckButton;
-
-public class MainActivityAdmin extends AppCompatActivity {
-    //Variables
-    ElasticCheckButton recuperar;
+public class MainActivityRestablecerClave extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_admin);
+        setContentView(R.layout.activity_main_restablecer_clave);
         //Titulo de la aplicacion o action bar que lo centre
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.txt_titulo);
 
         //Flecha volver
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        recuperar=(ElasticCheckButton) findViewById(R.id.btnRContraseña);
-        getRecuperar();
-    }
-
-    //Metodo para el boton recuperar contraseña
-    private void getRecuperar() {
-        recuperar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivityAdmin.this, MainActivityRestablecerClave.class);
-                Toast.makeText(getApplicationContext(), "Restablece tu clave",
-                        Toast.LENGTH_SHORT).show();
-                startActivity(i);
-            }
-        });
     }
 
     //Metodo para la flecha y volver atras
