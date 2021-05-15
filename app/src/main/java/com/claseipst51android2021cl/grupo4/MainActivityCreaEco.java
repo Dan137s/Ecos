d7692af6925953c10dfd6a92794dac56147af282
D4n137s <ossandonrojasdaniel@gmail.com>
2021-05-15 01:28:41 -0400

    V.1.23 logueando + grabadora

--- app/src/main/java/com/claseipst51android2021cl/grupo4/MainActivityCreaEco.java
+++ app/src/main/java/com/claseipst51android2021cl/grupo4/MainActivityCreaEco.java
@@ -9,17 +9,28 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.skydoves.elasticviews.ElasticCheckButton;
 
+import java.io.IOException;
+
 public class MainActivityCreaEco extends AppCompatActivity {
 
+
     ElasticCheckButton consularLatLong;
+    private MediaRecorder grabacion;
+    private String archivoSalida = null;
+    private Button btn_recorder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +41,21 @@ public class MainActivityCreaEco extends AppCompatActivity {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.txt_titulo);
 
-        //Latitud y longitud "Localización"
-        getLocalizacion();
-
         //para la flecha volver
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        btn_recorder =(Button)findViewById(R.id.btn_rec);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivityCreaEco.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.RECORD_AUDIO}, 1000);
+        }
         
     }
 
-    private void getLocalizacion() {
-        int permiso = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
-        //Este bloque consulta por el permiso si es denegado o no
-        if(permiso == PackageManager.PERMISSION_DENIED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
-            }else{
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
-            }
-        }
-    }
 
     //Metodo para regresar atras
     @Override
@@ -60,4 +67,45 @@ public class MainActivityCreaEco extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    public void Recorder(View view){
+        if(grabacion == null) {
+            archivoSalida = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Grabacion.mp3";
+            grabacion = new MediaRecorder();
+            grabacion.setAudioSource(MediaRecorder.AudioSource.MIC);
+            grabacion.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            grabacion.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            grabacion.setOutputFile(archivoSalida);
+
+            try {
+                grabacion.prepare();
+                grabacion.start();
+            } catch (IOException e) {
+            }
+            btn_recorder.setBackgroundResource(R.drawable.recr);
+            Toast.makeText(getApplicationContext(), "Grabando...",
+                    Toast.LENGTH_SHORT).show();
+        }else if (grabacion != null){
+            grabacion.stop();
+            grabacion.release();
+            grabacion = null;
+            btn_recorder.setBackgroundResource(R.drawable.stop_rec);
+            Toast.makeText(getApplicationContext(), "Grabacion finalizada...",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void reproducir(View view){
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        try{
+            mediaPlayer.setDataSource(archivoSalida);
+            mediaPlayer.prepare();
+        } catch (IOException e){
+        }
+        mediaPlayer.start();
+        Toast.makeText(getApplicationContext(), "Reproduciendo...",
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+
 }
