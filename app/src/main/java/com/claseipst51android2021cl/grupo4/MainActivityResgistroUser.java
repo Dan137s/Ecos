9cdcd294388a16f87dd7f285df343987df7fa2ab
D4n137s <ossandonrojasdaniel@gmail.com>
2021-05-23 01:25:48 -0400

    V.1.34 registro y restablecer

--- app/src/main/java/com/claseipst51android2021cl/grupo4/MainActivityResgistroUser.java
+++ app/src/main/java/com/claseipst51android2021cl/grupo4/MainActivityResgistroUser.java
@@ -1,10 +1,12 @@
 package com.claseipst51android2021cl.grupo4;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,6 +44,10 @@ public class MainActivityResgistroUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_resgistro_user);
+
+        //Titulo de la aplicacion o action bar que lo centre
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.txt_titulo);
 
         //Creando la instancia con la variable de firebase
         mAuth = FirebaseAuth.getInstance();
@@ -82,7 +88,20 @@ public class MainActivityResgistroUser extends AppCompatActivity {
             }
         });
 
+        //Flecha atras
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    //Metodo para volver atras de la flechita back
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void registerUser(){
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
