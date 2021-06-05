8b9d8396416983c876aef7a20b630af92d0f0ba3
D4n137s <ossandonrojasdaniel@gmail.com>
2021-06-04 23:48:26 -0400

    V.1.39 Grabadora

--- app/src/main/java/com/claseipst51android2021cl/grupo4/MainActivity.java
+++ app/src/main/java/com/claseipst51android2021cl/grupo4/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i1 = new Intent(MainActivity.this, MainActivityInicio.class);
+                Intent i1 = new Intent(MainActivity.this, MenuActivityInvitado.class);
                 startActivity(i1);
                 finish();
             }
