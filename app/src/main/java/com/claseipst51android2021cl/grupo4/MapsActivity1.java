c2764f1d3b66ceda30558562d2a29fbbfc3e07c0
D4n137s <ossandonrojasdaniel@gmail.com>
2021-06-09 21:08:56 -0400

    Ecos V1.49 Puntos de ubicación

--- app/src/main/java/com/claseipst51android2021cl/grupo4/MapsActivity1.java
+++ app/src/main/java/com/claseipst51android2021cl/grupo4/MapsActivity1.java
@@ -34,7 +34,7 @@ public class MapsActivity1 extends FragmentActivity implements OnMapReadyCallbac
         binding = ActivityMaps1Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtiene SupportMapFragment y recibe una notificación cuando el mapa esté listo para ser utilizado.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -80,7 +80,9 @@ public class MapsActivity1 extends FragmentActivity implements OnMapReadyCallbac
             @Override
             public void onLocationChanged(Location location) {
                 LatLng miUbicacion = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.addMarker(new MarkerOptions().position(miUbicacion).title("Ubicación actual"));
+                //Marcador de la ub. actual
+                mMap.addMarker(new MarkerOptions().position(miUbicacion).title("Ubicación actual").icon(BitmapDescriptorFactory.fromResource(R.drawable.marca3)));
+                //Foco de la camara según la ubicación
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(miUbicacion));
                 CameraPosition cameraPosition = new CameraPosition.Builder()
                         .target(miUbicacion)
@@ -112,7 +114,7 @@ public class MapsActivity1 extends FragmentActivity implements OnMapReadyCallbac
 
         // Ubicación Pais Chile principal no Australia, sidney ya bueno disney
         LatLng CpCL = new LatLng(-33.43782949348624, -70.65044066638902);
-        mMap.addMarker(new MarkerOptions().position(CpCL).title("Chile").snippet("Capital de chile, Santiago Región Metropolitana").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+        mMap.addMarker(new MarkerOptions().position(CpCL).title("Chile").snippet("Capital de chile, Santiago Región Metropolitana").icon(BitmapDescriptorFactory.fromResource(R.drawable.chile)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(CpCL));
 
         //Este es para centrar la vista o camara al cargar el mapa en una ubicacion predeterminada + un zoom
